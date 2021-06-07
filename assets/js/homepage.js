c130fd5df450825fab05394663d871a929adfcb6
Denisse Rivera <denisse_alejandrar@hotmail.com>
2021-06-07 15:42:09 -0500

    included search form

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,8 +1,20 @@
-var getUserRepos = function() {
-    var response = fetch("https://api.github.com/users/octocat/repos").then(function(response) {
-        response.json().then(function(data) {
-          console.log(data);
-        });
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+      response.json().then(function(data) {
+        console.log(data);
       });
-    }
-  getUserRepos()
+    });
+  };
+
+var formSubmitHandler = function(event) {
+    event.prevendDefault ();
+    console.log(event);
+};
+userFormEl.addEventListener("submit", formSubmitHandler);
